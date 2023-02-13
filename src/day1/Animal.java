236da1a7abcf242fb21cd0faa1ee23c5193720a9
Hugo Blandin <hu.blandin@gmail.com>
2023-02-13 10:36:17 +0100

    cration constructeur

--- src/day1/Animal.java
+++ src/day1/Animal.java
@@ -1,5 +1,11 @@
 package day1;
 
 public class Animal {
-
+	//1) instance
+	String nom;
+	int age;
+	//création d'un constructeur
+	public Animal() {
+		System.out.println("Constructeur d'un animal");
+	}
 }
--- src/day1/Application.java
+++ src/day1/Application.java
@@ -3,7 +3,13 @@ package day1;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello JAVA");// TODO Auto-generated method stub
+		//System.out.println("Hello JAVA");// TODO Auto-generated method stub
+		// 1) Création d'une instance
+		Animal a1 = new Animal();
+		Animal a2 = new Animal();
+		System.out.println(a1.nom);
+		System.out.println(a1.nom);
+		System.out.println(a1.age);
 
 	}
 
