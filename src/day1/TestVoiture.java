5c20892c28a8accdd37faed31adbf3e1af6c5ad1
Hugo Blandin <hu.blandin@gmail.com>
2023-02-13 12:57:06 +0100

    creation de ma fonction de saisi

--- src/day1/TestVoiture.java
+++ src/day1/TestVoiture.java
@@ -3,9 +3,34 @@ import java.time.LocalDate;
 import java.util.Scanner;
 public class TestVoiture {
 
+	public static Voiture saisiDataVoiture(Scanner sc) {
+		
+		System.out.println("tapez les information de votre voiture : ");
+		System.out.println("****");
+		System.out.println("tapez la marque : ");
+
+		String marque = sc.nextLine();
+		//System.out.println("marque = "+marque);
+		System.out.println("tapez le modele : ");
+		String modele = sc.nextLine();
+		System.out.println("Taper la date immatriculation sous la forme : 2010-10-1");
+		String ld = sc.nextLine();
+		
+		LocalDate dateImmat = LocalDate.parse(ld);
+		
+		System.out.println("tapez la vitesse max : ");
+		int vitesseMax = sc.nextInt();
+		/*
+		System.out.println("Marque = "+marque);
+		System.out.println("Modele = "+modele);
+		System.out.println("DateImmat = "+dateImmat);
+		System.out.println("VitesseMax = "+vitesseMax);
+		*/
+		return new Voiture(marque,modele,dateImmat,vitesseMax);
+	}
 	public static void main(String[] args) {
 		/*
-		 * //nbreVoiture = 0
+		 //nbreVoiture = 0
 		Voiture.getNbreVoitures();
 		Voiture v1 = new Voiture("MarioKart", "kart", LocalDate.of(2015, 10, 1), 3000);
 		//nbreVoiture = 1
@@ -22,24 +47,12 @@ public class TestVoiture {
 		
 		Voiture.getNbreVoitures(); // appel à la  méthode de classe
 		*/
-		System.out.println("tapez les information de votre voitur : ");
-		System.out.println("****");
-		System.out.println("tapez la marque : ");
 		Scanner sc = new Scanner(System.in);
-		String marque = sc.nextLine();
-		//System.out.println("marque = "+marque);
-		System.out.println("tapez le modele : ");
-		String modele = sc.nextLine();
-		System.out.println("tapez la date d'immatriculation : ");
-		String ld = sc.nextLine();
-		LocalDate dateImmat = LocalDate.parse(ld);
-		System.out.println("tapez la vitesse max : ");
-		int vitesseMax = sc.nextInt();
-		System.out.println("Marque = "+marque);
-		System.out.println("Modele = "+modele);
-		System.out.println("DateImmat = "+dateImmat);
-		System.out.println("VitesseMax = "+vitesseMax);
-
+		Voiture v1 = saisiDataVoiture(sc);
+		System.out.println(v1);
+		sc.nextLine();
+		Voiture v2 = saisiDataVoiture(sc) ;
+		System.out.println(v2);
 		
 	}
 
