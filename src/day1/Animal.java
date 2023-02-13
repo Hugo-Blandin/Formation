448eff133e8b507480ed649248b02930c9be3bbe
Hugo Blandin <hu.blandin@gmail.com>
2023-02-13 10:57:30 +0100

    ajout d'un nom, un age et un localDate

--- src/day1/Animal.java
+++ src/day1/Animal.java
@@ -1,16 +1,39 @@
 package day1;
 
+import java.time.LocalDate;
+
 public class Animal {
-	//1) instance
+	// 1)Attributs d'instance (propriétés ou caractéristiques)
 	String nom;
 	int age;
-	//création d'un constructeur
-	public Animal() {
-		System.out.println("Constructeur d'un animal");
+	LocalDate dateNaissance;
+	
+	// 2)Attribut de classe : c'est une propriété liée à la classe
+	static int AgeMax = 150;
+	
+	
+	// 3)Les contructeurs
+	//Constructeur sans paramètres
+	public Animal()  // le constructeur par défaut(sans paramètres)
+	{
+		System.out.println("Construction d'un Animal");
 	}
-	public Animal(String nom, int age) {
+	//Constructeur avec paramètres
+	public Animal(String nom, int age, LocalDate ld)
+	{
 		System.out.println("Construction d'un Animal avec deux paramèters");
 		this.nom = nom;
 		this.age = age;
+		this.dateNaissance = ld;
 	}
+	
+	// 4) Les méthodes d'instance
+	
+	public void info()
+	{
+		System.out.println("Nom : "+this.nom);
+		System.out.println("Age : "+this.age);
+		System.out.println("Date de naissance : "+this.dateNaissance);
+	}
+
 }
--- src/day1/Application.java
+++ src/day1/Application.java
@@ -1,19 +1,35 @@
 package day1;
 
+import java.time.LocalDate;
+
 public class Application {
 
 	public static void main(String[] args) {
-		//System.out.println("Hello JAVA");// TODO Auto-generated method stub
-		// 1) Création d'une instance
-		Animal a1 = new Animal();
-		Animal a2 = new Animal();
-		System.out.println(a1.nom); //null
-		System.out.println(a1.age); //0
+		//System.out.println("Hello Java");
 		
-		Animal a3 = new Animal("Rex",3);
-		System.out.println("Nom : " + a3.nom);
-		System.out.println("Age : " + a3.age);
-
+		// 1) Création d'une instance
+		
+		Animal a1 = new Animal(); // appel au contructeur par défaut
+		
+		Animal a2 = new Animal(); // appel au contructeur par défaut
+		
+		System.out.println(a1.nom);  //null
+		System.out.println(a1.age);  // 0
+        
+		
+		LocalDate naissance = LocalDate.of(2022, 7, 20);
+		Animal a3 = new Animal("Rex", 1, naissance);
+		a3.info();
+		a1.info();
+		
+		//System.out.println("Nom : " + a3.nom);
+		//System.out.println("Age : " + a3.age);
+		//System.out.println("Date Naissance : " + a3.dateNaissance);
+		
+		System.out.println(Animal.AgeMax);
+		
+		//System.out.println(a1.AgeMax);
+		//System.out.println(a2.AgeMax);
 	}
 
 }
